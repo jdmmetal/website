1cb1dc1b319cdf5cc108a34867646598011f1c4c
Namito Yokota <namitoyokota@icloud.com>
2024-02-25 20:31:02 -0600

    removed animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule],
+    imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
 })
