38a188cb7c3ba2e32d2019ab348da8f3a6c09993
Namito Yokota <namitoyokota@icloud.com>
2024-03-08 21:12:55 -0600

    added scroll reveal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgsRevealModule } from 'ngx-scrollreveal';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, NgsRevealModule],
     providers: [],
     bootstrap: [AppComponent],
 })
