e3c064008e1273c635b8f7a6999894f7d07876aa
Juan Mayz <jfmayz@crimson.ua.edu>
2024-04-12 23:55:22 -0400

    added new pics / some in carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,26 @@ export class AppComponent {
 
     public slides = [
         { src: '/assets/site-pics/COPPER/BARLEY/Barley1.jpg' },
-        { src: '/assets/site-pics/COPPER/T-BIRCH/TBirch3.jpg' },
         { src: '/assets/site-pics/COPPER/BARLEY/Barley2.jpg' },
-    ];
+        { src: '/assets/site-pics/COPPER/T-BIRCH/TBirch2.jpg' },
+        { src: '/assets/site-pics/COPPER/T-BIRCH/TBirch3.jpg' },
+        // { src: '/assets/site-pics/COPPER/BERRYCANDY/BerryCandy1.jpg' },
+        // { src: '/assets/site-pics/COPPER/BERRYCANDY/BerryCandy2.jpg' },
+        // { src: '/assets/site-pics/COPPER/CLOVE/Clove2.jpg' },
+        // { src: '/assets/site-pics/COPPER/CLOVE/Clove1.jpg' },
+        // { src: '/assets/site-pics/COPPER/HONEY/Honey1.jpg' },
+        // { src: '/assets/site-pics/COPPER/HONEY/Honey3.jpg' },
+        // { src: '/assets/site-pics/COPPER/TALK/Talk1.jpg' },
+        // { src: '/assets/site-pics/COPPER/OCEAN/Ocean3.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/TENSE/Tense4.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/TENSE/Tense5.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/UBC/Tense5.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/UBC/UBC1.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/UBC/UBC2.jpg' },
+        // { src: '/assets/site-pics/ALUMINIUM/UBC/UBC3.jpg' },
 
+
+    ];
+    
     constructor() {}
 }
